bfdc68b3229d2e2fe520dbdc31276cc67529c0a4
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-07 19:41:56 -0300

    variaveis 2.0

--- aprendendojava.js
+++ aprendendojava.js
@@ -5,3 +5,28 @@ let xablau;
 let value = null;
 
 console.log(age);
+console.log(fullName);
+console.log(isValid);
+console.log(xablau);
+console.log(value);
+
+if (true) {
+    // Início do escopo do if
+    var userAge = '20';
+    console.log(userAge); // 20
+    // Fim do escopo do if
+  }
+  
+  console.log(userAge); // 20
+
+  const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+
+console.log(favoriteLanguage); 
+// TypeError: Assignment to constant variable.
+
+let patientId = 50;
+let isEnrolled = true;
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientId); //number
