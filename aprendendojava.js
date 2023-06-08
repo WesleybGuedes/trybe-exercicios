c3f69587503251a231ec3ea28cd43243e54fdc25
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-08 00:44:12 -0300

    desenvolvendo if or else

--- aprendendojava.js
+++ aprendendojava.js
@@ -30,3 +30,28 @@ let isEnrolled = true;
 let patientEmail = 'ana@email.com';
 
 console.log(typeof patientId); //number
+
+const age = 87;
+
+if (age >= 18) {
+  console.log('Maior de idade');
+} else {
+  console.log('Menor de idade');
+}
+
+
+let currentHour = 16;
+let message = "";
+
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+  message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+  message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+  message = "Hora do almoço!!!";
+} else if(currentHour >= 4 && currentHour < 11) {
+  message = "Hmmm, cheiro de café recém-passado";
+}
+console.log(message);
