bc6f999f6d5416c9e5abf412de9b40d38b3b9764
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-10 21:32:45 -0300

    funçao e uma merda

---
+++ functions.js
@@ -0,0 +1,12 @@
+function somaArray(arrai) {
+  let sum = 0;
+        
+  for (let index = 0; index < arrai.length; index += 1) {
+    sum += arrai[index];
+  }
+
+  return sum;
+}
+
+const meuArray = [1, 2, 3, 4, 5];
+console.log(somaArray(meuArray));
